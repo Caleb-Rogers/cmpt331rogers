1484f8b74c236f51a1f878dcce0ce68a8cbfaa03
Caleb-Rogers <Caleb.Rogers1@marist.edu>
2022-03-01 22:42:38 -0500

    Finished Caesar Cipher

--- Fortran/caesar_cipher.f90
+++ Fortran/caesar_cipher.f90
@@ -1,63 +1,60 @@
-program caesar_cipher
+program Caesar_Cipher
   implicit none
+ 
+  ! Initialization
+  character(43) :: cipherStr = "Dude Wheres My Car"
+  integer, parameter :: shift = 4  
+ 
+  write(*, "(2a)") "Welcome to Caesar Ciphers with Fortran!"
   
-  ! Initializations
-  character :: cipherStr = "DUDE"
-  integer :: shift = 4
-
-  print *, "Welcome to Caesar Ciphers with Fortran!"
-
   ! Call Caesar Cipher Encryption Method
   call encrypt(cipherStr, shift)
-  print *, "Encrypted Cipher Value: ", cipherStr
-
+  write(*, "(2a)") "Encrypted Cipher Value: ", cipherStr
+  
   ! Call Caesar Cipher Decryption Method
   call decrypt(cipherStr, shift)
-  print *, "Decrypted Cipher Value: ", cipherStr
-
+  write(*, "(2a)") "Decrypted Cipher Value: ", cipherStr
+  
   ! Call Caesar Cipher Solve Method
-  call solve(cipherStr, 26)
+  call solve("DUDE", 26)
+ 
+contains
 
-end program caesar_cipher
-
-
-subroutine encrypt(toCipher, shift) 
-  implicit none
-
+ 
+subroutine encrypt(toCipher, shift)
   character :: toCipher
   integer :: shift
   integer :: i
-
-  do i=1, len(toCipher)
-    if (iachar(toCipher(i:i)) == 32) then
-      toCipher(i:i) = achar(32)
-    else
-      toCipher(i:i) = achar(modulo(iachar(toCipher(i:i)) - 65 + shift, 26) + 65)
-    end if
+ 
+  do i = 1, len(toCipher)
+    select case(toCipher(i:i))
+      case ('A':'Z')
+        toCipher(i:i) = achar(modulo(iachar(toCipher(i:i)) - 65 + shift, 26) + 65)
+      case ('a':'z')
+        toCipher(i:i) = achar(modulo(iachar(toCipher(i:i)) - 97 + shift, 26) + 97)
+    end select
   end do
   
-end subroutine encrypt
-
-
-subroutine decrypt(ciphered, shift) 
+end subroutine
+ 
+ 
+subroutine decrypt(ciphered, shift)
   implicit none
-
-  character :: ciphered 
+  character :: ciphered
   integer :: shift
   integer :: reverse
-
+  
   reverse = (shift * (-1))
-	call encrypt(ciphered, reverse)
-
-end subroutine decrypt
+  call encrypt(ciphered, reverse)
+  
+end subroutine
 
 
 subroutine solve(cipher, shiftLoop)
   implicit none
-
-  character :: cipher
+  character(4) :: cipher
   integer :: shiftLoop
-  character :: newCiphered
+  character(4) :: newCiphered
   integer :: i
   
   do i = 0, shiftLoop
@@ -65,4 +62,8 @@ subroutine solve(cipher, shiftLoop)
     call encrypt(newCiphered, i)
     write(*, *) "Caesar ", i, ": ", newCiphered
   end do
+  
 end subroutine
+
+
+end program Caesar_Cipher
