72fc2a544481b1d17795893c61aefba1e927e643
Caleb-Rogers <Caleb.Rogers1@marist.edu>
2022-03-02 00:19:09 -0500

    Better looking encrypt method; but broken-ish

--- Pascal/caesar_cipher.pas
+++ Pascal/caesar_cipher.pas
@@ -3,30 +3,16 @@ program CaesarCipher(output);
   var cipherStr: String;
   var shift : Integer;
 
-function encrypt(toCipher: String; shift:Integer): String;
+function encrypt(toCipher: String; shift: Integer): String;
   var
-    i, temp: Integer;
-    toUpperStr: String;
+    i: Integer;
   begin
-    toUpperStr := UpCase(toCipher);
-    for i := 1 to length(toUpperStr) do
-    
-    begin
-      temp := (byte(toUpperStr[i]) - 65 + shift) mod Integer(26);
-      if (temp < 0) then
-        begin
-          temp := temp + 26;
-        end;
-      temp := temp + 65;
-
-      if (byte(toUpperStr[i]) = 32) then
-        begin
-          temp := 32;
-        end;
-      toUpperStr[i] := chr(temp);
-    end;
-
-    encrypt := toUpperStr;
+    for i := 1 to length(toCipher) do
+      case toCipher[i] of
+        'A'..'Z': toCipher[i] := chr(ord('A') + (ord(toCipher[i]) - ord('A') + shift) mod 26);
+        'a'..'z': toCipher[i] := chr(ord('a') + (ord(toCipher[i]) - ord('a') + shift) mod 26);
+      end;
+    encrypt := toCipher;
   end;
 
 
