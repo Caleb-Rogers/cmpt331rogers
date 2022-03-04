3a5e39a457546d26ff6952cf7f02c8fa19ab4c27
Caleb-Rogers <rogerscaleb2019@gmail.com>
2022-03-04 03:35:17 -0500

    Encrypt Works; Now for Decrypt

--- COBOL/caesar_cipher.cob
+++ COBOL/caesar_cipher.cob
@@ -10,10 +10,10 @@ WORKING-STORAGE SECTION.
     01 shift          PIC 9(4) VALUE 4.
     01 loop           PIC 9(8) VALUE 26.
     01 negativeShift  PIC 9(4) VALUE 4.
-    01 temp           PIC x(50).
+    01 ciphered           PIC x(50).
     01 i              PIC 9(3).
     01 a              PIC 9(3).
-    01 chars          PIC 9(1).
+    01 chars          PIC x(1).
 
 
 PROCEDURE DIVISION.
@@ -23,41 +23,48 @@ Begin.
     DISPLAY "Welcome to Caesar Ciphers with COBOL!"
 
     PERFORM Encrypt.
-    DISPLAY "Encrypted Cipher Value: " cipherStr
+    DISPLAY "Encrypted Cipher Value: " ciphered
 
     PERFORM Decrypt.
-    DISPLAY "Decrypted Cipher Value: " cipherStr
+    DISPLAY "Decrypted Cipher Value: " ciphered
     STOP RUN.
 
 
 Encrypt.
     MOVE Function Upper-case(cipherStr) to cipherStr
+
+    IF shift >= 26
+       MOVE FUNCTION MOD(shift, 26) to shift
+    END-IF
+		
     PERFORM VARYING i FROM 1 BY 1 UNTIL i > FUNCTION LENGTH(cipherStr)
-       IF cipherStr IS NOT EQUAL TO SPACE
-           MOVE cipherStr (i:1) to chars
-           IF (FUNCTION ORD(chars) + shift) <= FUNCTION ORD("Z")
-                   MOVE FUNCTION CHAR(FUNCTION ORD(chars) + chars) to cipherStr 
-               ELSE
-                   MOVE FUNCTION CHAR(FUNCTION ORD("A") 
-                   + ((FUNCTION ORD(chars) + shift) - 1) - FUNCTION ORD("Z")) to
-		       END-IF
-		    END-IF
+	     IF cipherStr(i:1) IS NOT EQUAL TO SPACE
+				MOVE cipherStr (i:1) to chars
+				IF (FUNCTION ORD(chars) + shift) <= FUNCTION ORD("Z")
+					MOVE FUNCTION CHAR(FUNCTION ORD(chars) + shift) to ciphered (i:1)
+				ELSE
+					MOVE FUNCTION CHAR(FUNCTION ORD("A") 
+					+ ((FUNCTION ORD(chars) + shift) - 1) - FUNCTION ORD("Z")) to ciphered (i:1
+				END-IF
+			END-IF
 		END-PERFORM
-    STOP RUN.
+      .
 
 
 Decrypt.
-    SUBTRACT 26 FROM shift GIVING negativeShift
-    MOVE Function Upper-case(cipherStr) to cipherStr
+    IF shift >= 26
+       MOVE FUNCTION MOD(shift, 26) to shift
+    END-IF
+		
     PERFORM VARYING i FROM 1 BY 1 UNTIL i > FUNCTION LENGTH(cipherStr)
-       IF cipherStr IS NOT EQUAL TO SPACE
-           MOVE cipherStr (i:1) to chars
-           IF (FUNCTION ORD(chars) + negativeShift) <= FUNCTION ORD("Z")
-                   MOVE FUNCTION CHAR(FUNCTION ORD(chars) + chars) to cipherStr 
-               ELSE
-                   MOVE FUNCTION CHAR(FUNCTION ORD("A") 
-                   + ((FUNCTION ORD(chars) + negativeShift) - 1) - FUNCTION ORD(
-		       END-IF
-		    END-IF
+	     IF cipherStr(i:1) IS NOT EQUAL TO SPACE
+				MOVE cipherStr (i:1) to chars
+				IF (FUNCTION ORD(chars) + shift) <= FUNCTION ORD("Z")
+					MOVE FUNCTION CHAR(FUNCTION ORD(chars) + shift) to ciphered (i:1)
+				ELSE
+					MOVE FUNCTION CHAR(FUNCTION ORD("A") 
+					+ ((FUNCTION ORD(chars) + shift) - 1) - FUNCTION ORD("Z")) to ciphered (i:1
+				END-IF
+			END-IF
 		END-PERFORM
-    STOP RUN.
+      .
