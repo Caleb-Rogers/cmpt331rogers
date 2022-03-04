188a2689bb80c36d596fbe347d55a23e6a9ee0a8
Caleb-Rogers <rogerscaleb2019@gmail.com>
2022-03-04 07:23:20 -0500

    Comments for Scala; OFFICIALLY DONE

--- Scala/caesar_cipher.scala
+++ Scala/caesar_cipher.scala
@@ -1,16 +1,19 @@
 object CaesarCipher {
     def encrypt(toCipher: String, shift: Int): String = {
 		var ciphered: String = "";
-		
         for(i <- 0 to (toCipher.length() - 1)) {
+                // Account for uppercase letters
                 if (toCipher.charAt(i) >= 'A' && toCipher.charAt(i) <= 'Z') {
+                    // If no whitespace, shift character
                     if(!((toCipher.charAt(i)).isWhitespace)) {
                         ciphered = ciphered + (((toCipher.charAt(i).toInt + shift - 65) % 26).toInt + 65).toChar.toString();
                     }
+                    // Else whitespace, append space 
                     else {
                         ciphered = ciphered + toCipher.charAt(i);
                     }
                 }
+                // Account for lowercase letters
                 else {
                     if(!((toCipher.charAt(i)).isWhitespace)) {
                         ciphered = ciphered + (((toCipher.charAt(i).toInt + shift - 97) % 26).toInt + 97).toChar.toString();
@@ -25,6 +28,7 @@ object CaesarCipher {
 
 
     def decrypt(ciphered: String, shift: Int): String = {
+        // Shifts back to original
         var negativeShift: Int = (26 - shift)
         return encrypt(ciphered, negativeShift)
     }
