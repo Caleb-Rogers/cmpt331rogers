be4989ecea94ab79dfc7d03a064cebf8bb53ff28
Caleb-Rogers <rogerscaleb2019@gmail.com>
2022-03-04 03:40:53 -0500

    Its something... decrypt still needs fixed

--- COBOL/caesar_cipher.cob
+++ COBOL/caesar_cipher.cob
@@ -11,6 +11,7 @@ WORKING-STORAGE SECTION.
     01 loop           PIC 9(8) VALUE 26.
     01 negativeShift  PIC 9(4) VALUE 4.
     01 ciphered           PIC x(50).
+    01 deciphered           PIC x(50).
     01 i              PIC 9(3).
     01 a              PIC 9(3).
     01 chars          PIC x(1).
@@ -52,18 +53,20 @@ Encrypt.
 
 
 Decrypt.
+    MOVE Function Upper-case(cipherStr) to cipherStr
+
     IF shift >= 26
        MOVE FUNCTION MOD(shift, 26) to shift
     END-IF
 		
     PERFORM VARYING i FROM 1 BY 1 UNTIL i > FUNCTION LENGTH(cipherStr)
-	     IF cipherStr(i:1) IS NOT EQUAL TO SPACE
-				MOVE cipherStr (i:1) to chars
-				IF (FUNCTION ORD(chars) + shift) <= FUNCTION ORD("Z")
-					MOVE FUNCTION CHAR(FUNCTION ORD(chars) + shift) to ciphered (i:1)
+	 		IF cipherStr(i:1) IS NOT EQUAL TO SPACE
+				MOVE cipherStr (i:1) to c
+				IF (FUNCTION ORD(c) - numShift) >= FUNCTION ORD("A")
+					MOVE FUNCTION CHAR(FUNCTION ORD(c) - numShift) to deciphered (i:1)
 				ELSE
-					MOVE FUNCTION CHAR(FUNCTION ORD("A") 
-					+ ((FUNCTION ORD(chars) + shift) - 1) - FUNCTION ORD("Z")) to ciphered (i:1
+					MOVE FUNCTION CHAR(FUNCTION ORD("Z") 
+					- ((numShift - 1) - (FUNCTION ORD(c) - FUNCTION ORD("A")))) to deciphered (
 				END-IF
 			END-IF
 		END-PERFORM
