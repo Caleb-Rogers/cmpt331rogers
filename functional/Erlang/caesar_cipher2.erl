2f1aef56e2a6bfd1cd3933149e0422836fe99c88
Caleb-Rogers <Caleb.Rogers1@marist.edu>
2022-04-21 01:26:15 -0400

    Erlang done
    Worked perfect; make sure to check over and comment

--- functional/Erlang/caesar_cipher2.erl
+++ functional/Erlang/caesar_cipher2.erl
@@ -1,23 +1,35 @@
--module(caesar_cipher2).
+-module(prog).
 -export([encrypt/2, decrypt/2, solve/2, main/0]).
 
-encrypt(Str, ShftAmt) ->
-    io:fwrite("Encrypt...\n").
 
-decrypt(Str, ShftAmt) ->
-    io:fwrite("Decrypt...\n").
+offset(Char,Key) when (Char >= $A) and (Char =< $Z) or
+                   (Char >= $a) and (Char =< $z) ->
+  Offset = $A + Char band 32,
+  N = Char - Offset,
+  Offset + (N + Key) rem 26;
+offset(Char, _Key) ->
+  Char.
 
-solve(Str, MaxShftAmt) ->
-    io:fwrite("Solve...\n").
+encrypt(ToCipher, Shift) ->
+    lists:map(fun(Char) -> offset(Char, Shift) end, ToCipher).
+
+decrypt(Ciphered, Shift) ->
+    encrypt(Ciphered, -Shift).
+
+solve(Cipher, 0) ->
+ 	io:format("~s~n", [string:concat("Caesar 0: ",string:to_upper(Cipher))]);
+solve(Cipher, Shift) when Shift > 0 -> 
+	io:format("~s~n", [lists:concat(["Caesar ", Shift, ": ", encrypt(Cipher, Shift)])]),
+	solve(Cipher, (Shift-1)).
 
 
 %% Main Function
 main() ->
     % Initialization
-    CipherStr = "the quick brown fox jumped over the lazy dog",
+    CipherStr = "Dude Wheres My Car",
 	Shift = 4,
 
-    io:fwrite("Welcome to Caesar Ciphers with Erlang!\n").
+    io:fwrite("Welcome to Caesar Ciphers with Erlang!\n"),
 
     % Call Caesar Cipher Encryption Method
     encrypt(CipherStr, Shift),
