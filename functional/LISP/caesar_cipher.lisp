3eebb2f4946f83fae3e3249868909b6c7d1fa559
Caleb-Rogers <rogerscaleb2019@gmail.com>
2022-04-21 23:40:48 -0400

    LISP is done
    Might need some cleaning up but works!

--- functional/LISP/caesar_cipher.lisp
+++ functional/LISP/caesar_cipher.lisp
@@ -1,38 +1,26 @@
-(defun shifter(char shift)
-  (if (= (char-code char) 32) (code-char 32)
-    (code-char (+ (mod (+ (- (char-code char) 65) shift) 26) 65))))
-
+(defun offset(chars shift)
+	(if (= (char-code chars) 32) (code-char 32)
+	  (code-char (+ (mod (+ (- (char-code chars) 65) shift) 26) 65))))
 
 (defun encrypt (toCipher shift)
-  ;(map 'string #'(lambda (char) (shifter char shift)) toCipher))
-
-)
-
+  (map 'string #'(lambda (ch) (offset ch shift)) (string-upcase toCipher)))
 
 (defun decrypt (ciphered shift) (encrypt ciphered (- shift)))
-
 
 (defun solve (cipher shift) 
   (loop for i from 0 to shift
     do (format t "Caesar ~D: ~a~%" i (encrypt cipher i))))
 
+;; Main ;;
+; Initialization & Encryption & Decryption
+(let* ((cipherStr "Dude Wheres My Car")
+      (shift 4)
+      (ciphered (encrypt cipherStr shift))
+      (deciphered (decrypt ciphered shift)))
 
-;; Main Function
-(defun main()
-    ; Initialization
-    (setq cipherStr "Dude Wheres My Car")
-    (setq shift 4)
-
-    (print ("Welcome to Caesar Ciphers with LISP!"))
-
-    ; Call Caesar Cipher Encryption Method
-    (setq ciphered (encrypt cipherStr shift))
-    ;(format t "Encrypted Cipher Value: ~a ~%" ciphered)
-    (format t "Encrypted Cipher Value: ~a" ciphered)
-
-    ; Call Caesar Cipher Decryption Method
-    ;;(setq deciphered (encrypt ciphered shift))
-    ;;(format t "Encrypted Cipher Value: ~a ~%" deciphered)
-        
-    ; Call Caesar Cipher Solve Method
-    ;;(solve "DUDE" 26))
+  ; Output results & call Solve
+  (format t "Welcome to Caesar Ciphers with LISP! ~%")
+  (format t "Encrypted Cipher Value: ~a ~%" ciphered)
+  (format t "Decrypted Cipher Value: ~a ~%" deciphered)
+  (solve "DUDE" 26)
+)
