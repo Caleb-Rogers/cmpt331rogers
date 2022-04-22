7233e57aed6708e7f39a6c4fa636364389f83cfa
Caleb-Rogers <rogerscaleb2019@gmail.com>
2022-04-21 21:56:16 -0400

    Erlang cleaned up... but far from done
    This works as is! BUT referencing rosetta code...

--- functional/Erlang/caesar_cipher.erl
+++ functional/Erlang/caesar_cipher.erl
@@ -2,22 +2,24 @@
 -export([encrypt/2, decrypt/2, solve/2, main/0]).
 
 
-offset(Char,Key) when (Char >= $A) and (Char =< $Z) or
-                   (Char >= $a) and (Char =< $z) ->
-  Offset = $A + Char band 32,
-  N = Char - Offset,
-  Offset + (N + Key) rem 26;
-offset(Char, _Key) ->
-  Char.
+shifter(Char,Shift)
+    % Uppercase
+    when (Char >= $A) and (Char =< $Z)
+    % Lowercase
+    or (Char >= $a) and (Char =< $z) ->
+        Offset = $A + Char band 32,
+        Offset + ((Char - Offset) + Shift) rem 26;
+        % recursive call
+        shifter(Char, _Shift) -> Char.
 
 encrypt(ToCipher, Shift) ->
-    lists:map(fun(Char) -> offset(Char, Shift) end, ToCipher).
+    lists:map(fun(Char) -> shifter(Char, Shift) end, ToCipher).
 
 decrypt(Ciphered, Shift) ->
     encrypt(Ciphered, -Shift).
 
 solve(Cipher, 0) ->
- 	io:format("~s~n", [string:concat("Caesar 0: ",string:to_upper(Cipher))]);
+    io:format("~s~n", [string:concat("Caesar 0: ",string:to_upper(Cipher))]);
 solve(Cipher, Shift) when Shift > 0 -> 
 	io:format("~s~n", [lists:concat(["Caesar ", Shift, ": ", encrypt(Cipher, Shift)])]),
 	solve(Cipher, (Shift-1)).
