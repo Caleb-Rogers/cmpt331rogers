67be154eeb97658a9050af2c7ff7d6c06cff5f4f
Caleb-Rogers <Caleb.Rogers1@marist.edu>
2022-04-22 05:48:28 -0400

    Encrypt working (I think); Decrypt broken

--- functional/JavaScript/caesar_cipher.js
+++ functional/JavaScript/caesar_cipher.js
@@ -1,19 +1,14 @@
 function shifter(char, shift) {
-  if(char.trim() === '') {
-    console.log("space: " + char);
-  }
-  else {
-    let code = char.charCodeAt(0);
-
+  let ciphered_char = " ";
+  if(char.trim() != '') {
     if (char === char.toUpperCase()) {
-      console.log("upper: " + char);
-      console.log(String.fromCharCode(((char.charCodeAt(0) - 65 + shift) % 26) + 65));
+      ciphered_char = String.fromCharCode(((char.charCodeAt(0) - 65 + shift) % 26) + 65);
     }
     else {
-      console.log("lower: " + char);
-      console.log(String.fromCharCode(((char.charCodeAt(0) - 97 + shift) % 26) + 97));
+      ciphered_char = String.fromCharCode(((char.charCodeAt(0) - 65 + shift) % 26) + 65);
     }
   }
+  return ciphered_char;
 }
 
 
@@ -21,6 +16,10 @@ function encrypt(toCipher, shift) {
     const toCipher_arr = toCipher.split('');
     let shiftAmt = shift % 26;
     const ciphered_arr = toCipher_arr.map(char => shifter(char, shiftAmt));
+    var ciphered = ciphered_arr.join("");
+    
+    console.log(ciphered);
+    return ciphered;
 }
 
 
@@ -38,15 +37,16 @@ function solve(cipher, shift) {
 // Initialization
 var cipherStr = "Dude Wheres My Car";
 var shift = 4;
-var ciphered = "";
 
 console.log("Welcome to Caesar Ciphers with JavaScript!");
 
 // Call Caesar Cipher Encryption Method
-encrypt(cipherStr, shift);
+var ciphered = encrypt(cipherStr, shift);
+console.log("Encrypted Cipher Value: " + ciphered);
 
 // Call Caesar Cipher Decryption Method
-//decrypt(ciphered, shift);
+var deciphered = decrypt(ciphered, shift);
+console.log("Decrypted Cipher Value: " + deciphered);
 
 // Call Caesar Cipher Solve Method
 //solve("DUDE", 26);
