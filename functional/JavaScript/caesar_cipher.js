0d8414179665b60cf088270b1be106d9d0b5bf9e
Caleb-Rogers <Caleb.Rogers1@marist.edu>
2022-04-22 05:31:06 -0400

    Might have a keeper with this JS update

--- functional/JavaScript/caesar_cipher.js
+++ functional/JavaScript/caesar_cipher.js
@@ -1,26 +1,52 @@
-//check if letter is uppercase
-function isUpperCase(str) {
-  return str === str.toUpperCase();
-}
+function shifter(char, shift) {
+  if(char.trim() === '') {
+    console.log("space: " + char);
+  }
+  else {
+    let code = char.charCodeAt(0);
 
-//decipher the string
-let ceaserCipher = (str, key) => {
-  let decipher = "";
-
-  //decipher each letter
-  for (let i = 0; i < str.length; i++) {
-    //if letter is uppercase then add uppercase letters
-    if (isUpperCase(str[i])) {
-      decipher += String.fromCharCode(
-        ((str.charCodeAt(i) + key - 65) % 26) + 65
-      );
-    } else {
-      //else add lowercase letters
-      decipher += String.fromCharCode(
-        ((str.charCodeAt(i) + key - 97) % 26) + 97
-      );
+    if (char === char.toUpperCase()) {
+      console.log("upper: " + char);
+      console.log(String.fromCharCode(((char.charCodeAt(0) - 65 + shift) % 26) + 65));
+    }
+    else {
+      console.log("lower: " + char);
+      console.log(String.fromCharCode(((char.charCodeAt(0) - 97 + shift) % 26) + 97));
     }
   }
+}
 
-  return decipher;
-};
+
+function encrypt(toCipher, shift) {
+    const toCipher_arr = toCipher.split('');
+    let shiftAmt = shift % 26;
+    const ciphered_arr = toCipher_arr.map(char => shifter(char, shiftAmt));
+}
+
+
+function decrypt(ciphered, shift) {
+    encrypt(ciphered, -shift);
+}
+
+
+function solve(cipher, shift) {
+    
+}
+
+
+/** Main **/
+// Initialization
+var cipherStr = "Dude Wheres My Car";
+var shift = 4;
+var ciphered = "";
+
+console.log("Welcome to Caesar Ciphers with JavaScript!");
+
+// Call Caesar Cipher Encryption Method
+encrypt(cipherStr, shift);
+
+// Call Caesar Cipher Decryption Method
+//decrypt(ciphered, shift);
+
+// Call Caesar Cipher Solve Method
+//solve("DUDE", 26);
