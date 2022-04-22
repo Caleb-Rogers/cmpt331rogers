b852ce85eb574531e0f4379bb2520a2f5815074b
Caleb-Rogers <Caleb.Rogers1@marist.edu>
2022-04-22 04:18:31 -0400

    Mostly encrypting/decrypting & solve "works"
    Solve is decrypting right but broken output

--- functional/Scala/caesar_cipher.scala
+++ functional/Scala/caesar_cipher.scala
@@ -1,4 +1,7 @@
 object CaesarCipher {
+
+    var cipherStr = ""
+
     def encrypt(toCipher: String, shift: Int) = {
         var shiftAmt: Int = shift % 26
         toCipher.map(char => shifter(char, shiftAmt))
@@ -9,21 +12,38 @@ object CaesarCipher {
         if (char >= 'A' && char <= 'Z') {
             // If no whitespace, shift character
             if(!(char.isWhitespace)) {
-                print((((char.toInt + shift - 65) % 26).toInt + 65).toChar.toString());
+                print((((char.toInt + shift - 65) % 26).toInt + 65).toChar.toString())
+                this.cipherStr += (((char.toInt + shift - 65) % 26).toInt + 65).toChar.toString()
             }
             // Else whitespace, append space 
             else {
                 print(" ")
+                this.cipherStr += (" ")
             }
         }
         // Account for lowercase letters
         else {
             if(!(char.isWhitespace)) {
-                print((((char.toInt + shift - 97) % 26).toInt + 97).toChar.toString());
+                print((((char.toInt + shift - 97) % 26).toInt + 97).toChar.toString())
+                this.cipherStr += (((char.toInt + shift - 65) % 26).toInt + 65).toChar.toString()
             }
             else {
                 print(" ")
+                this.cipherStr += (" ")
             }
+        }
+    }
+
+
+    def decrypt(shift: Int) = {
+		encrypt(this.cipherStr, (-shift))
+	}
+
+
+    def solve(cipher: String, shift: Int): Unit = {
+        println("Caesar " + shift + ": " + encrypt(cipher, shift))
+        if(shift > 0) {
+            solve(cipher, shift-1)
         }
     }
 
@@ -37,13 +57,16 @@ object CaesarCipher {
         println("Welcome to Caesar Ciphers with Scala!")
         
         // Call Caesar Cipher Encryption Method
+        print("Encrypted Cipher Value: ")
         encrypt(cipherStr, shift)
+        println("")
   
         // Call Caesar Cipher Decryption Method
-        //var deciphered = decrypt(ciphered, shift)
-        //println("Decrypted Cipher Value: " + deciphered)
+        print("Decrypted Cipher Value: ")
+        decrypt(shift)
+        println("")
       
         // Call Caesar Cipher Solve Method
-        //solve("DUDE", 26)
+        solve("DUDE", 26)
     }
 }
